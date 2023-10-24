fca22af9919cf4e317a9549be5ee99ebc1bb283e
yixiangTW <1540086396@qq.com>
2023-10-24 17:13:35 +0800

    feat: Add findValue in I10N_DATA

--- packages/data-context/src/actions/WizardActions.ts
+++ packages/data-context/src/actions/WizardActions.ts
@@ -481,11 +481,27 @@ const FIXTURE_DATA = {
 const I10N_DATA = `export const L10n = {
   commonMsg: (locale) => {
     const _SOURCE = require(\`../common-messages/\${locale}.json\`);
-    return (key) => _SOURCE[key];
+    return (key) => findValue(_SOURCE, key);
   },
   specialMsg: (product, version, component, locale) => {
     const _SOURCE = require(\`../common-messages/\${product}/\${version}/\${component}/\${locale}.json\`);
-    return (key) => _SOURCE[key];
+    return (key) => findValue(_SOURCE, key);
+  },
+  findValue: (source, key) => {
+    if (source.hasOwnProperty(key)) {
+      return source[key];
+    }
+
+    for (const prop in source) {
+      if (typeof source[prop] === 'object') {
+        const result = findValue(source[prop], key);
+        if (result !== undefined) {
+          return result;
+        }
+      }
+    }
+
+    return key;
   }
 };`
 
